d6516b4d1c5d6fefd6a2a52325868e903d1e4b81
dan naor pc <dannaor2001@gmail.com>
2023-12-31 19:35:30 +0200

    commit

--- src/serve.ts
+++ src/serve.ts
@@ -17,21 +17,25 @@ restliClient
     console.error(error)
   })
 
-// // comment on a random post -
-// const myURN= `urn:li:person:NJHtUMA1Xv`
-// const postURN = 'urn:li:activity:7146564642033639424'
-// restliClient.create({
-//   resourcePath: `/socialActions/comments`,
-//   entity: {
-//     actor: myURN,
-//     object:postURN,
-//     message: {
-//       text: 'great post!',
-//     },
-//   },
-//   accessToken: process.env.THREE_LEGGED_TOKEN,
-//   versionString: '202207'
-// }).then(response => {
-//   // const createdEntityId = response.createdEntityId;
-//   console.log(response)
-// });
+// post a text share -
+const myURN= `urn:li:person:NJHtUMA1Xv`
+restliClient.create({
+  resourcePath: `/posts`,
+  entity: {
+    author:myURN,
+    lifecycleState:"PUBLISHED",
+    ShareContent:{
+      shareCommentary:{
+        text:"Hello World! This is my first Share on LinkedIn!"
+      }
+    },
+    visibility: {
+      MemberNetworkVisibility:"PUBLIC"
+    }
+  },
+  accessToken: process.env.THREE_LEGGED_TOKEN,
+  versionString: '202303'
+}).then(response => {
+  // const createdEntityId = response.createdEntityId;
+  console.log(response)
+});
